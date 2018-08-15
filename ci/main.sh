42b5920c9903384abce2009ed2284a6641af8c88
Millioner <millioner.bbb@gmail.com>
2018-08-15 13:25:03 +0300

    moreee fixes for main.sh

--- ci/main.sh
+++ ci/main.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 echo "foooo!!!"
+
+cd $WORKSPACE
+
 mkdir -p coverage_report
 mkdir -p reports
-printenv
+
 pwd
+ls -la
+
+
 py.test src/
+
+
 exit $result
